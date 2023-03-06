e6944450aaac228297e4bcde23824d880964d711
indahrf <99266430+indahrf@users.noreply.github.com>
2023-03-06 17:28:32 +0700

    Update Main.py

--- PYTHON/Program/Main.py
+++ PYTHON/Program/Main.py
@@ -1,7 +1,7 @@
 
 
 
-# Saya Indah Resti Fauzi NIM 2103507 mengerjakan soal Latihan 1 dalam mata kuliah Desain dan Pemrograman Berorientasi Objek 
+# Saya Indah Resti Fauzi NIM 2103507 mengerjakan soal Latihan 4 dalam mata kuliah Desain dan Pemrograman Berorientasi Objek 
 # untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
 
 #Import file
